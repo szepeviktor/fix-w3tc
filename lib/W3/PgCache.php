815a0b1a94053ebc694077db77b74b6e009c975d
amiga-500 <core905@gmail.com>
2016-09-12 21:23:17 -0400

    Url Parsing Fix for Page Cache Flushing
    Corrects a url parsing flaw when flushing a page.  It now is able to
    properly strip out anomalies like a port #.

--- lib/W3/PgCache.php
+++ lib/W3/PgCache.php
@@ -965,8 +965,10 @@ class W3_PgCache {
         $encryption = '', $compression = '', $content_type = '', $request_uri = '') {
 
         if ($request_uri){
-            $is_ssl_request = ( strtolower(substr($request_uri, 0, 8)) == 'https://' );
-            $key = substr( $request_uri, $is_ssl_request ? 8 : 7 );
+            $uri = parse_url($request_uri);
+		  $key =  $uri["host"].
+		          (isset($uri["path"])?$uri["path"]:"").
+		          (isset($uri["query"])?"?".$uri["query"]:"");
         } else {
             $key = $this->_request_host . $this->_request_uri;
 		}
