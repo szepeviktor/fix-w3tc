723515bb55752b2db2a76b0babea7cca9a0dfd3e
nigrosimone <nigro.simone@gmail.com>
2017-01-06 15:14:10 +0100

    fixs

--- tests/w3-advanced-cache-test.php
+++ tests/w3-advanced-cache-test.php
@@ -44,25 +44,26 @@ class W3_Advanced_Cache_Test extends WP_UnitTestCase {
 		if( !defined('WP_DEBUG') ){
 			define('WP_DEBUG', true);
 		}
-		
-		if(WP_CACHE !== true){
-			throw new Exception('WP_CACHE not true (needed for enable the cache)');
-		}
-		if(WP_DEBUG !== true){
-			throw new Exception('WP_DEBUG not true (needed for check the cache)');
-		}
-		
+
 		if( !file_exists(WP_CONTENT_DIR.'/advanced-cache.php') ){
 			if( !copy(dirname(dirname(__FILE__)).'/wp-content/advanced-cache.php', WP_CONTENT_DIR.'/advanced-cache.php') ){
 				throw new Exception('Failed copy advanced-cache.php in WP_CONTENT_DIR');
 			}
 		}
 		
+		if( !WP_CACHE ){
+			include( WP_CONTENT_DIR . '/advanced-cache.php' ); // fuck the system!
+			//throw new Exception('WP_CACHE not true (needed for enable the cache)');
+		}
+		if( !WP_DEBUG ){
+			throw new Exception('WP_DEBUG not true (needed for check the cache)');
+		}
+		
 		$this->config  = w3_instance('W3_Config');
 		$this->factory = $this->__get('factory');
 		
 		// create a post and set the url
-		$this->test_post = get_post($this->factory->post->create(array('post_title' => 'Test Title', 'post_content' => 'Test Content')));
+		$this->test_post = get_post($this->factory->post->create(array('post_title' => 'TestTitle', 'post_content' => 'TestContent')));
 	}
 	
 	/**
@@ -87,6 +88,10 @@ class W3_Advanced_Cache_Test extends WP_UnitTestCase {
 		if( is_null($post_url) ){
 			$post_url = get_permalink($this->test_post->ID);
 		}
+		
+		if( empty($post_url) ){
+			throw new Exception('call_url empty post_url');
+		}
 
 		$ch = curl_init();
 		
@@ -97,7 +102,11 @@ class W3_Advanced_Cache_Test extends WP_UnitTestCase {
 		$result = curl_exec($ch);
 		
 		if ( curl_errno($ch) ){
-			throw new Exception(curl_error($ch));
+			throw new Exception('curl error:' . curl_error($ch));
+		}
+		
+		if ( !$result ){
+			throw new Exception('curl error on result');
 		}
 		
 		curl_close($ch);
@@ -111,13 +120,26 @@ class W3_Advanced_Cache_Test extends WP_UnitTestCase {
 	 * @return int
 	 */
 	private function cache_date($content){
-		return strtotime(substr($string, -42, 20)); // 2017-01-06 14:01:59 by W3 Total Cache -->
+		
+		if( empty($content) ){
+			throw new Exception('content is empty');
+		}
+		
+		if( strpos($content, 'TestTitle') === false ){
+			throw new Exception('TestTitle not found in content: '.$content);
+		}
+		
+		if( strpos($content, 'by W3 Total Cache') === false ){
+			throw new Exception('W3 Total Cache debug info not found in content: '.$content);
+		}
+		
+		return strtotime(substr($content, -42, 20)); // 2017-01-06 14:01:59 by W3 Total Cache -->
 	}
     
     /**
      * Check the cache
      */
-    public function test_cache($data)
+    public function test_cache()
     {
         $first_request_time  = $this->cache_date($this->call_url());
         
