d810ba1619955eb9c38c01aef0d899d8c5928b63
Stephen J. Butler <stephen.butler@gmail.com>
2017-03-24 02:31:05 -0500

    Increate the media library export timeout.
    5 seconds is not nearly enough time to upload a batch of 25
    attachments. For example, if it's 25 image attachments then that
    is potentially 125 different files.

--- Cdn_AdminActions.php
+++ Cdn_AdminActions.php
@@ -118,7 +118,7 @@ class Cdn_AdminActions {
 		$results = array();
 
 		$w3_plugin_cdn->export_library( $limit, $offset, $count, $total,
-			$results, time() + 5 );
+			$results, time() + 120 );
 
 		$response = array(
 			'limit' => $limit,
