89714ebef4a921dc842db311afd57f79a937a0f9
nigrosimone <nigro.simone@gmail.com>
2017-07-04 20:50:31 +0200

    test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,11 +17,13 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	copy('/tmp/wordpress/wp-content/plugins/w3-total-cache/wp-content/advanced-cache.php', '/tmp/wordpress/wp-content/advanced-cache.php');
-	copy('/tmp/wordpress/wp-content/plugins/w3-total-cache/wp-content/db.php',             '/tmp/wordpress/wp-content/db.php');
-	copy('/tmp/wordpress/wp-content/plugins/w3-total-cache/wp-content/object-cache.php',   '/tmp/wordpress/wp-content/object-cache.php');
+	$plugin_dir = dirname( dirname( __FILE__ ) );
 	
-	require dirname( dirname( __FILE__ ) ) . '/w3-total-cache.php';
+	copy($plugin_dir.'/wp-content/advanced-cache.php', '/tmp/wordpress/wp-content/advanced-cache.php');
+	copy($plugin_dir.'/wp-content/db.php',             '/tmp/wordpress/wp-content/db.php');
+	copy($plugin_dir.'/wp-content/object-cache.php',   '/tmp/wordpress/wp-content/object-cache.php');
+	
+	require $plugin_dir. '/w3-total-cache.php';
 	update_option( 'active_plugins', 'w3-total-cache/w3-total-cache.php' );
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
